27dd68ce3f95f754605f0aeb88145a751fcf3e43
DianaBeta <dianabetancourt7@gmail.com>
2021-04-06 20:32:05 +0200

    feat: route from clien to server

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -3,8 +3,32 @@ function handleSubmit(event){
  
  let destination = document.getElementById('destination').value;
  let date = document.getElementById('date').value;
- 
-}
 
+      
+postData('http://localhost:8081/addCity', {destination: destination})
+    
+} 
 
-export { handleSubmit }
+const postData = async ( url = '', data = {})=>{
+  console.log(data);
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+   // Body data type must match "Content-Type" header        
+    body: JSON.stringify(data), 
+  });
+
+    try {
+      const newData = await response.json();
+      console.log(newData);
+      return newData;
+    }catch(error) {
+      console.log("error", error);
+    }
+  }
+
+export { handleSubmit } 
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,11 @@
+const fetch = require("node-fetch");
+const dotenv = require ('dotenv');
+dotenv.config();
+var APIusername = process.env. APIUSERNAME;
 
 
-
+//emty JS project to act as endpoint for all routes
+const projectData = {};
 // Express to run server and routes
 const express= require('express');
 // Start up an instance of app
@@ -17,7 +22,7 @@ app.use(bodyParser.json());
 const cors = require ('cors');
 app.use(cors());
 // get the info from the index file 
-app.get('/', function (req, res) {
+app.get('/all', function (req, res) {
     res.sendFile('src/client/views/index.html')
     
 })
@@ -26,6 +31,45 @@ app.use(express.static('dist'))
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+    console.log('listening on port 8081!')
 })
-                   
+
+//axions instead of fetch
+const axios = require('axios');
+//post route
+//API call 
+/*const getDataFromGeoNames= async (username,city)=>{
+    const url=`http://api.geonames.org/searchJSON?q=${city}&maxRows=10&=${username}`;
+//http://api.geonames.org/search?q=london&maxRows=10&username=dianabetancourt
+    try{
+        return await axios.get(url)
+                .then(res=>{
+                    return {
+                        lat:res.data.geonames[0].lat,
+                        lng:res.data.geonames[0].lng
+                    }
+                });
+    } catch(error){
+        console.log("error", error)
+    }
+}
+*/
+
+app.post('/addCity', function(req,res){
+    let data =req.body.destination;
+    newEntry = {
+        city:req.body.destination
+    }
+    projectData.newEntry = newEntry;
+    console.log(projectData)
+    res.send(projectData);
+    
+})
+
+
+/*getDataFromGeoNames(APIusername,req.body.destination).then(
+    apiResponse => {
+    console.log(apiResponse)
+    res.json(apiResponse);
+    });
+})*/
