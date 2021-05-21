adef379f8f59cb06ce3ed0d0c36e8d2b657ffd0a
DianaBeta <dianabetancourt7@gmail.com>
2021-05-21 13:22:22 +0200

    feat: "procfile and ect deleted"

--- src/server/start.js
+++ src/server/start.js
@@ -1,8 +1,8 @@
 // start.js
 // designates what port the app will listen to for incoming requests
 
-//let port = 8081
-var port = process.env.PORT || 8081;
+let port = 8081
+//var port = process.env.PORT || 8081;
 const app = require('./server.js')
 app.listen(port)
 console.log(`listening on port ${port}`)
